8f08bdd074ae42bb79efeb77f4dcb59048d156d9
Michael Rago <michael.rago@gmail.com>
2022-03-19 15:13:24 -0400

    basic functionality complete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,6 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import BreweryList from './components/BreweryList'
 import Notification from './components/Notification'
-import GoogleMap from './components/GoogleMap'
-import Marker from './components/Marker'
-import { Wrapper } from "@googlemaps/react-wrapper";
-
-const markers = [
-  { lat: 48.7601273, lng: -122.4645645 },
-];
 
 const App = () => {
   const [breweries, setBreweries] = useState([])
@@ -37,17 +30,6 @@ const App = () => {
       </div>
       <Notification message={errorMessage} />
       <BreweryList breweries={breweries} />
-      <div style={{ display: "flex", height: "50vh", width: "50vw" }}>
-        <Wrapper apiKey={"AIzaSyDdK9NheJPLy2jDcxdoNoQj0FkWVM4HhZk"}>
-          <GoogleMap
-            center={{ lat: 48.7601273, lng: -122.4645645 }}
-            zoom={18}
-            style={{ flexGrow: "1", height: "100%" }}
-          >
-            <Marker position={{ lat: 48.7601273, lng: -122.4645645 }} />;
-          </GoogleMap>
-        </Wrapper>
-      </div>
     </div>
   )
 }
--- src/components/BreweryDetail.js
+++ src/components/BreweryDetail.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import GoogleMap from './GoogleMap'
+import Marker from './Marker'
+import { Wrapper } from "@googlemaps/react-wrapper";
+
 
 const BreweryDetail = props => {
     if (!props.show) {
         return null
     }
+
+    const coordinates = {
+        lat: parseFloat(props.brewery.latitude),
+        lng: parseFloat(props.brewery.longitude),
+    }
+
     return (
         <div className="modal" onClick={props.onClose}>
             <div className="modal-content" onClick={e => e.stopPropagation()}>
@@ -12,6 +22,17 @@ const BreweryDetail = props => {
                 </div>
                 <div className="modal-body">
                     <p>{props.brewery.name}</p>
+                    <div style={{ display: "flex", height: "35vh", width: "35vw" }}>
+                        <Wrapper apiKey={"AIzaSyDdK9NheJPLy2jDcxdoNoQj0FkWVM4HhZk"}>
+                        <GoogleMap
+                            center={coordinates}
+                            zoom={18}
+                            style={{ flexGrow: "1", height: "100%" }}
+                        >
+                            <Marker position={coordinates} />;
+                        </GoogleMap>
+                        </Wrapper>
+                    </div>
                 </div>
                 <div className="modal-footer">
                     <button onClick={props.onClose} className="button">Close</button>
