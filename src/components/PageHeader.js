11c91e1f93844b1701c0c622c2a9a1d01d0e19e9
Michael Rago <michael.rago@gmail.com>
2022-03-20 16:13:10 -0400

    fixed header image

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -4,7 +4,6 @@ import bhambay from "../bhambay.jpg"
 const PageHeader = () => {
     return (
         <div className="page-header">
-            <h1 className="header-text">Breweries of Bellingham</h1>
             <img className="main-image" src={bhambay} />
         </div>
     )
